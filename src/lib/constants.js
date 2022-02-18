f66acda00a0b354bba5a013e9b7694d4a7265b96
Cesar Ramos <cesar.2023.ramos@gmail.com>
2022-02-18 18:06:27 -0300

    Updates

--- src/lib/constants.js
+++
@@ -1,3 +0,0 @@
-export const MIN_CHARACTER = 5;
-export const VALIDATE_EMAIL = /\S+@\S+\.\S+/;
-export const TAG_INITIAL = 'Alimentação';
--- src/services/walletAPI.js
+++
@@ -1,9 +0,0 @@
-const WALLET_API = 'https://economia.awesomeapi.com.br/json/all';
-
-const getCurrentCoins = async () => {
-  const response = await fetch(WALLET_API);
-  const result = await response.json();
-  return result;
-};
-
-export default getCurrentCoins;
